829968e3f5f59669d0f298fb7b9c5026b194bd38
KenwoodFox <kenwood364@gmail.com>
2023-07-25 15:22:44 -0400

    Patch issue compiling on windows

--- firmware/build_flags.py
+++ firmware/build_flags.py
@@ -5,42 +5,48 @@ from datetime import date
 
 today = date.today()
 
-revision = (
-    subprocess.check_output(
-        [
-            "git",
-            "describe",
-            "--abbrev=8",
-            "--dirty",
-            "--always",
-            "--tags",
-        ]
-    )
-    .strip()
-    .decode("utf-8")
-)
 
-host = (
-    subprocess.check_output(
-        [
-            "hostname",
-        ]
+try:
+    revision = (
+        subprocess.check_output(
+            [
+                "git",
+                "describe",
+                "--abbrev=8",
+                "--dirty",
+                "--always",
+                "--tags",
+            ]
+        )
+        .strip()
+        .decode("utf-8")
     )
-    .strip()
-    .decode("utf-8")
-)
 
-username = (
-    subprocess.check_output(
-        [
-            "id",
-            "-u",
-            "-n",
-        ]
+    host = (
+        subprocess.check_output(
+            [
+                "hostname",
+            ]
+        )
+        .strip()
+        .decode("utf-8")
     )
-    .strip()
-    .decode("utf-8")
-)
+
+    username = (
+        subprocess.check_output(
+            [
+                "id",
+                "-u",
+                "-n",
+            ]
+        )
+        .strip()
+        .decode("utf-8")
+    )
+except:
+    revision = "Unknown"
+    host = "Unknown"
+    username = "Unknown"
 
 # Cleanup CI
 if username == "root":
